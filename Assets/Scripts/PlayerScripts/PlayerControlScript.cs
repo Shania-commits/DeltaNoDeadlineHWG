695e730fbb0ec1bdcaff765049dd48849d2a7fd9
Shania-commits <74222546+Shania-commits@users.noreply.github.com>
2026-02-24 13:22:18 -0500

    Player Code and Camera additions

---
+++ Assets/Scripts/PlayerScripts/PlayerControlScript.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    public float speed = 8f;
+    public float turnSpeed = 5f;
+    private Rigidbody rb;
+
+    [SerializeField] Transform cam;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        //Moves player
+        float movementX = Input.GetAxis("Horizontal");
+        float movementZ = Input.GetAxis("Vertical");
+
+        Vector3 cameraForward = cam.forward;
+        Vector3 cameraRight = cam.right;
+
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+
+        Vector3 forwardRelative = movementZ * cameraForward;
+        Vector3 rightRelative = movementX * cameraRight;
+
+        Vector3 moveDirection = forwardRelative + rightRelative;
+
+        Vector3 movement = new Vector3(movementZ, 0f, movementX);
+
+        rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.z);
+        rb.AddForce(movement * speed);
+
+        //Turns during movement
+        //float turn = movementX * turnSpeed * Time.fixedDeltaTime;
+        //Quaternion tRotation = Quaternion.Euler(0f, turn, 0f);
+        //rb.MoveRotation(rb.rotation * tRotation);
+    }
+}
