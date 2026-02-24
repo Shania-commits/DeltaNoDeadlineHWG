6d5489b8f66577628cc99d318140ac827608252a
Shania-commits <74222546+Shania-commits@users.noreply.github.com>
2026-02-24 15:40:50 -0500

    Rotation Functional
    Rotation is functional but needs to be refined; Might be messing with keyboard controls

--- Assets/Scripts/PlayerScripts/PlayerControlScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerControlScript.cs
@@ -2,8 +2,8 @@ using UnityEngine;
 
 public class PlayerControlScript : MonoBehaviour
 {
-    public float speed = 8f;
-    public float turnSpeed = 5f;
+    public float speed = 13f;
+    public float turnSpeed = 50f;
     private Rigidbody rb;
 
     [SerializeField] Transform cam;
@@ -26,6 +26,7 @@ public class PlayerControlScript : MonoBehaviour
         float movementX = Input.GetAxis("Horizontal");
         float movementZ = Input.GetAxis("Vertical");
 
+        /*Turn with camera
         Vector3 cameraForward = cam.forward;
         Vector3 cameraRight = cam.right;
 
@@ -35,16 +36,16 @@ public class PlayerControlScript : MonoBehaviour
         Vector3 forwardRelative = movementZ * cameraForward;
         Vector3 rightRelative = movementX * cameraRight;
 
-        Vector3 moveDirection = forwardRelative + rightRelative;
+        Vector3 moveDirection = forwardRelative + rightRelative; */
 
-        Vector3 movement = new Vector3(movementZ, 0f, movementX);
+        Vector3 movement = new Vector3(movementX, 0f, movementZ);
 
-        rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.z);
+        //rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.z);
         rb.AddForce(movement * speed);
 
         //Turns during movement
-        //float turn = movementX * turnSpeed * Time.fixedDeltaTime;
-        //Quaternion tRotation = Quaternion.Euler(0f, turn, 0f);
-        //rb.MoveRotation(rb.rotation * tRotation);
+        float turn = movementX * turnSpeed * Time.fixedDeltaTime;
+        Quaternion tRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * tRotation);
     }
 }
