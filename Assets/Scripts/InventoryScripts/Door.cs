d75caa143671fe63ba887e82507fca00e91cac90
ElleDeRoin <107569262+ElleDeRoin@users.noreply.github.com>
2026-02-24 17:18:02 -0500

    Added Crowbar/Window Scripts

---
+++ Assets/Scripts/InventoryScripts/Door.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    private bool isOpening = false;
+    private bool playerInRange = false;
+    private PlayerInventory currentInventory;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerInventory inventory = other.GetComponent<PlayerInventory>();
+
+        if (inventory != null)
+        {
+            playerInRange = true;
+            currentInventory = inventory;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<PlayerInventory>() != null)
+        {
+            playerInRange = false;
+            currentInventory = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInRange && Input.GetKeyDown(KeyCode.X))
+        {
+            if (currentInventory.hasKey)
+            {
+                Debug.Log("Door unlocked!");
+                isOpening = true;
+            }
+            else
+            {
+                Debug.Log("Door is locked. Find the Crowbar.");
+            }
+        }
+
+        if (isOpening)
+        {
+            Debug.Log("Door is opening...");
+        }
+    }
+}
