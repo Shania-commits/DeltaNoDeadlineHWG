de7911d94e84edbae8ad098a7f8ff0e62555f98a
Shania-commits <74222546+Shania-commits@users.noreply.github.com>
2026-02-25 01:20:41 -0500

    Camera & Turning

--- Assets/Scripts/PlayerScripts/PlayerControlScript.cs
+++ Assets/Scripts/PlayerScripts/PlayerControlScript.cs
@@ -3,10 +3,8 @@ using UnityEngine;
 public class PlayerControlScript : MonoBehaviour
 {
     public float speed = 13f;
-    public float turnSpeed = 50f;
+    public float turnSpeed = 74f;
     private Rigidbody rb;
-
-    [SerializeField] Transform cam;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -23,29 +21,15 @@ public class PlayerControlScript : MonoBehaviour
     private void FixedUpdate()
     {
         //Moves player
-        float movementX = Input.GetAxis("Horizontal");
-        float movementZ = Input.GetAxis("Vertical");
+        float movementX = Input.GetAxis("Horizontal"); //Turns left and right
+        float movementZ = Input.GetAxis("Vertical"); //Forwaed and back
 
-        /*Turn with camera
-        Vector3 cameraForward = cam.forward;
-        Vector3 cameraRight = cam.right;
-
-        cameraForward.y = 0;
-        cameraRight.y = 0;
-
-        Vector3 forwardRelative = movementZ * cameraForward;
-        Vector3 rightRelative = movementX * cameraRight;
-
-        Vector3 moveDirection = forwardRelative + rightRelative; */
-
-        Vector3 movement = new Vector3(movementX, 0f, movementZ);
-
-        //rb.linearVelocity = new Vector3(moveDirection.x, rb.linearVelocity.y, moveDirection.z);
+        //Handles forward movement
+        Vector3 movement = transform.forward * movementZ;
         rb.AddForce(movement * speed);
 
-        //Turns during movement
-        float turn = movementX * turnSpeed * Time.fixedDeltaTime;
-        Quaternion tRotation = Quaternion.Euler(0f, turn, 0f);
+        //Handles turning
+        Quaternion tRotation = Quaternion.Euler(0f, movementX * turnSpeed * Time.fixedDeltaTime, 0f);
         rb.MoveRotation(rb.rotation * tRotation);
     }
 }
