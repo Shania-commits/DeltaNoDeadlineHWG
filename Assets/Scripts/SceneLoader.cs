2b3054e4007a781116019974014a1ddb62b7469f
MoonChild <phoebe.moon.sousa@gmail.com>
2026-02-26 17:19:23 -0500

    Add Settings scene and singleton SceneLoader
    Add a new Settings scene (Assets/Scenes/SettingScene.unity + .meta). Make SceneLoader a singleton (DontDestroyOnLoad, destroy duplicates) so it persists across scene loads. Tweak MainMenu UI RectTransform anchors/size values. Update EditorBuildSettings to include Assets/Scenes/LevelDesign.unity in the build list.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -3,6 +3,21 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
+    private static SceneLoader instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     public void LoadSceneByName(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
